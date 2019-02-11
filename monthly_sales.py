b9e05c1b84b186e49705b54424ce93f618b09e14
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-02-11 18:13:41 -0500

    Update monthly_sales.py

--- monthly_sales.py
+++ monthly_sales.py
@@ -7,11 +7,27 @@
 # File path = sales-201710.csv
 
 import matplotlib as plt
-
+import datetime as dt
 import pandas as pd
 from matplotlib import style
 
+#ALL Variables
+top_sellers = []
+total_sales = 0.0
+individual_sales = 0.0
+sales_price = 0.0
+matching_product = " "
+
 df = pd.read_csv("sales-201904.csv")
+
+#Gettign unique 
+products = df["product"].unique()
+
+print(type(products))
+
+print(products)
+
+total_sales = df['sales price'].sum()
 
 print("-----------------------")
 print("MONTH: March 2018")
@@ -20,7 +36,24 @@ print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+print("TOTAL MONTHLY SALES: " + str(total_sales))
+
+print("Product               Sum of sales price")
+
+    # matching_product = [p for p in products if p["id"] == selected_id]
+    # product = matching_product[0]["name"]
+    # matching_price = matching_product[0]["price"]
+
+
+matching_product = [p for p in products if p["product"] == "Khaki Pants"]
+
+print(matching_product["unit price"])
+
+# for index, row in df.iterrows():
+#     if row["product"] =="Khaki Pants":
+#         print(row["sales price"] )
+
+print(individual_sales)
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
@@ -31,7 +64,7 @@ print("  3) etc.")
 print("-----------------------")
 print("VISUALIZING THE DATA...")
 
-print(df)
+print(df['unit price'][0])
 
 # f= open("sales-201904.csv","r")
 # print(f.read())
