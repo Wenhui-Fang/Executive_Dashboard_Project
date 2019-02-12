9af3915f33fb8257ed2e67357b523f7cf4f71794
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-02-12 15:20:25 -0500

    Hard coded bar chart

--- monthly_sales.py
+++ monthly_sales.py
@@ -6,10 +6,13 @@
 
 # File path = sales-201710.csv
 
-import matplotlib as plt
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
 import datetime as dt
 import pandas as pd
-from matplotlib import style
+
 import operator
 
 #ALL Variables
@@ -20,7 +23,7 @@ sales_price = 0.0
 
 
 df = pd.read_csv("sales-201904.csv")
-total_monthly_sales = df['sales price'].sum
+total_monthly_sales = df['sales price'].sum()
 
 #Getting unique product name
 products = df["product"].unique()
@@ -38,8 +41,8 @@ for product_name in unique_product_list:
 
 top_sellers = sorted(top_sellers, key=operator.itemgetter("monthly_sales"), reverse=True)
 
-print(top_sellers)
 
+# print(top_sellers)
 
 
 print("-----------------------")
@@ -49,7 +52,7 @@ print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-print("TOTAL MONTHLY SALES: ")
+print("TOTAL MONTHLY SALES: " + str(total_monthly_sales))
 
 print("Product               Sum of sales price")
 
@@ -68,14 +71,38 @@ print("Product               Sum of sales price")
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
-print("  1) Button-Down Shirt: $6,960.35")
-print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
+
+for p in top_sellers:
+    print(p["name"] + "             " + str(p["monthly_sales"]))
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
 
-print(df['unit price'][0])
-
 # f= open("sales-201904.csv","r")
 # print(f.read())
+
+# TODO: make a chart
+
+# adapted from code posted to matplotlib Slack channel: https://georgetown-opim-py.slack.com/archives/CFZDKNKA4/p1549494877005200
+
+bar_data = [
+    {"genre": "Thriller", "viewers": 123456},
+    {"genre": "Mystery", "viewers": 234567},
+    {"genre": "Sci-Fi", "viewers": 987654},
+    {"genre": "Fantasy", "viewers": 876543},
+    {"genre": "Documentary", "viewers": 283105},
+    {"genre": "Action", "viewers": 544099},
+    {"genre": "Romantic Comedy", "viewers": 121212}
+]
+
+genres = []
+viewers = []
+
+for s in bar_data:
+  genres.append(s["genre"])
+  viewers.append(s["viewers"])
+
+plt.bar(genres, viewers)
+plt.ylabel("Viewers")
+plt.xlabel("Genre")
+plt.show()
